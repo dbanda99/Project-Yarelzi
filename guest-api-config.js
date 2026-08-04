7467109d138a950ab4943a4eb3400e813c564efc
dbanda99 <ldavidbandal@gmail.com>
2026-08-04 16:01:41 -0500

    0.0.8

--- guest-api-config.js
+++ guest-api-config.js
@@ -1,4 +1,4 @@
-window.GUEST_API_URL = "https://script.google.com/macros/s/AKfycbxHGoofY4w5eLrfS1kO2a3TADHbiVH5wsCxQAFXfxs3avOydT8Tz6iLeOPzaZqmfQhM/exec";
+window.GUEST_API_URL = "https://script.google.com/macros/s/AKfycbw9S_syCIU_LGaRo_KzTgAYGFQCnwaKytGTNg6WqXHdanqI-mBaYdJxvCw1f_bLK1A0/exec";
 
 // Live Server testing only: this guest is shown when no ?guest= value is present.
 window.GUEST_API_LOCAL_DEFAULT_GUEST = "1";
